bf0895d16776dfa4e54f280d91765058b054a735
Mg <gekoncze@seznam.cz>
2023-05-28 19:13:33 +0200

    refactored annotations

--- src/cz/mg/file/FileException.java
+++ src/cz/mg/file/FileException.java
@@ -1,8 +1,9 @@
 package cz.mg.file;
 
+import cz.mg.annotations.classes.Error;
 import cz.mg.annotations.requirement.Mandatory;
 
-public class FileException extends RuntimeException {
+public @Error class FileException extends RuntimeException {
     public FileException(@Mandatory String message, @Mandatory Throwable cause) {
         super(message, cause);
     }
